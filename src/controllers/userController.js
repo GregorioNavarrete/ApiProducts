3ce01d76546691de6c34200da588f7b9273b8078
francoled <ledesmaf659@gmail.com>
2024-03-24 21:49:53 -0300

    metodo constructor franco

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,21 @@
 const path = require('path');
 const userService = require('../data/userService');
 const fs = require('fs');
+const { name } = require('ejs');
+
+function user(name, last_name, email){
+  this.name = name
+  this.last_name = last_name
+  this.email = email
+}
+
+function url(puerto){
+  
+}
+
+
+
+
 
 const userController = {
 
@@ -8,9 +23,11 @@ const userController = {
         try {
          
           let users = await userService.getAll();
+          let newUsers = users.map(usuario=> new user(usuario.name, usuario.last_name, usuario.email))
           res.status(200).json({
             count: users.length,
-            users: users,
+            user: newUsers,
+            
             
           });
           
